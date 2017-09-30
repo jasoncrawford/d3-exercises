c54b7a8a119da1811f116448063f783c6d9e2ffb
Jason Crawford <jason@jasoncrawford.org>
2017-09-30 13:34:43 -0700

    Chart real data loaded from file

--- charts.js
+++ charts.js
@@ -11,6 +11,7 @@ charts.barChart = function (selector) {
   const padding = 2;
   const margin = 10;
   const scale = 10;
+  const textHeight = 18.5;
 
   let innerWidth = chartWidth - 2 * margin;
   let innerHeight = chartHeight - 2 * margin;
@@ -26,15 +27,37 @@ charts.barChart = function (selector) {
     .attr('width', innerWidth)
     .attr('height', innerHeight)
 
-  let scaleY = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, innerHeight])
+  let draw = data => {
+    let scaleY = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.patents)])
+      .range([0, innerHeight])
 
-  content.selectAll('rect').data(data)
-    .enter().append('rect')
-      .classed('bar', true)
-      .attr('x', (d, i) => i * (barSize + padding))
-      .attr('y', d => innerHeight - scaleY(d))
-      .attr('width', barSize)
-      .attr('height', scaleY)
+    content.append('text')
+      .attr('x', 0)
+      .attr('y', textHeight)
+      .text(`US utility patents, ${data[0].year} to ${data[data.length-1].year}`)
+
+    content.append('a')
+      .attr('href', 'https://www.datazar.com/file/f698590f4-2d24-4de4-918c-a198690bba2a')
+      .attr('target', '_blank')
+      .append('text')
+        .attr('x', 0)
+        .attr('y', 2 * textHeight)
+        .text('Source: Datazar')
+
+    content.selectAll('rect').data(data)
+      .enter().append('rect')
+        .classed('bar', true)
+        .attr('x', (d, i) => i * (barSize + padding))
+        .attr('y', d => innerHeight - scaleY(d.patents))
+        .attr('width', barSize)
+        .attr('height', d => scaleY(d.patents))
+  }
+
+  let transform = d => ({year: d['Calendar Year'], patents: +d['Utility Patents']})
+
+  d3.csv('data/us-patents.csv', transform, data => {
+    data = data.slice(0,25).reverse();
+    draw(data);
+  })
 }
