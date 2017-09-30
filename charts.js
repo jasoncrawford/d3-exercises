fb35cd4c3053cdb31b8485f322766e8627b42986
Jason Crawford <jason@jasoncrawford.org>
2017-09-30 12:18:02 -0700

    Actually draw bar chart based on data

--- charts.js
+++ charts.js
@@ -1,16 +1,26 @@
 window.charts = window.charts || {};
 let charts = window.charts;
 
+const chartWidth = 640;
+const chartHeight = 480;
+
 charts.barChart = function (selector) {
+  let data = [1, 1, 2, 3, 5, 8, 13];
+
+  const barSize = 20;
+  const padding = 2;
+  const scale = 10;
+
   let svg = d3.select(selector).append('svg')
     .classed('bar-chart', true)
-    .attr('width', 640)
-    .attr('height', 480)
+    .attr('width', chartWidth)
+    .attr('height', chartHeight)
 
-  svg.append('rect')
-    .classed('bar', true)
-    .attr('x', 10)
-    .attr('y', 10)
-    .attr('width', 20)
-    .attr('height', 100)
+  svg.selectAll('rect').data(data)
+    .enter().append('rect')
+      .classed('bar', true)
+      .attr('x', (d, i) => i * (barSize + padding))
+      .attr('y', d => chartHeight - d * scale)
+      .attr('width', barSize)
+      .attr('height', d => d * scale)
 }
