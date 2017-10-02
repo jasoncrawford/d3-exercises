1f1097035fedef1440e96653a4fa78f6c836357c
Jason Crawford <jason@jasoncrawford.org>
2017-10-02 00:22:29 -0700

    Debounce slider for smoothness/perf

--- charts.js
+++ charts.js
@@ -253,11 +253,14 @@ charts.scatterPlot = function (selector) {
     selection.exit().remove();
   }
 
+  let debouncedDraw = _.debounce(draw, 50);
+
   handle.call(d3.drag().on('drag', () => {
     let x = d3.event.x;
-    if (x < 0 || x > width) return;
+    if (x < 0) x = 0;
+    if (x > width) x = width;
     d3.select('.slider-handle').attr('cx', x);
-    draw();
+    debouncedDraw();
   }))
 
   // Ultimate format will be like:
